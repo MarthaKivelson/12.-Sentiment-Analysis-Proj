40a7dafb222afbbc7fde62861cb6cf6d6a3afddb
Uttam K P <95496600+SlimShadyAnonymous@users.noreply.github.com>
2025-09-05 15:08:17 +0530

    CICD run 1.7

--- src/data/data_ingestion.py
+++ src/data/data_ingestion.py
@@ -78,9 +78,9 @@ def main():
         test_size = params['data_ingestion']['test_size']
         # test_size = 0.2
         
-        df = load_data("notebooks\\data.csv")
-        # s3 = s3_connection.s3_operations("kputtprojbuc", "AKIA5G2BO7IUYIVSNDHS", "mc3BLTF5AS46ne55VdF+5Har8JeD49piPndoAb5K")
-        # df = s3.fetch_file_from_s3("data.csv")
+        #df = load_data("notebooks\\data.csv")
+        s3 = s3_connection.s3_operations("kputtprojbuc", "AKIA5G2BO7IUYIVSNDHS", "mc3BLTF5AS46ne55VdF+5Har8JeD49piPndoAb5K")
+        df = s3.fetch_file_from_s3("data.csv")
 
 
 
